5c540943eb2ef81bd8787c882762cb9e0b7ae3c6
Sitala <striboga@yandex.ru>
2018-02-28 22:03:31 +0200

    tiren

--- mybot/app.py
+++ mybot/app.py
@@ -2,10 +2,10 @@
 #!/usr/bin/python
 
 import os
-from gevent import monkey
-monkey.patch_all()
-from gevent.pywsgi import WSGIServer
-from geventwebsocket.handler import WebSocketHandler
+# from gevent import monkey
+# monkey.patch_all()
+# from gevent.pywsgi import WSGIServer
+# from geventwebsocket.handler import WebSocketHandler
 
 import os
 import cherrypy
@@ -13,9 +13,13 @@ import wsgigzip
 
 from mybot.project import app
 
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
 host = "0.0.0.0"
 port = int(os.environ.get("PORT", 5000))
-def_app = wsgigzip.GzipMiddleware(app)
+# def_app = wsgigzip.GzipMiddleware(app)
 
 # --------------------------------------------------------------
 # cherrypy.config.update({'server.socket_host': host,
@@ -51,16 +55,14 @@ def_app = wsgigzip.GzipMiddleware(app)
 # # app = wsgigzip.GzipMiddleware(bottle.default_app())
 #
 # # --------------------------------------------------------------
-# # cherrypy.config.update({'server.socket_host': host,
-# #                         'server.socket_port': port})
-# # cherrypy.tree.graft(def_app)
-# # cherrypy.engine.start()
-# # cherrypy.engine.block()
+cherrypy.config.update({'server.socket_host': host,
+                        'server.socket_port': port})
+cherrypy.tree.graft(app)
+cherrypy.engine.start()
+cherrypy.engine.block()
 # #----------------------------------------------------------------
 
-import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
 
-server = WSGIServer((host, port), def_app, handler_class=WebSocketHandler)
-server.serve_forever()
+
+# server = WSGIServer((host, port), def_app, handler_class=WebSocketHandler)
+# server.serve_forever()
