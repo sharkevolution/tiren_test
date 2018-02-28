66ddd8aea12bd41db6bd5d683f821e21eaaba602
Sitala <striboga@yandex.ru>
2018-02-28 22:11:55 +0200

    tiren

--- mybot/project/controllers/home.py
+++ mybot/project/controllers/home.py
@@ -25,20 +25,16 @@ def do_admin():
     redirect('/')
 
 
-@bottle.route('/api/v1/echo')
+@bottle.route('/api/v1/echo', method='POST')
 def do_echo():
     bottoken = '528159377:AAEI3Y3zTYv18e2qBp_nXBBMxLZU1uUhPHg'
-    baseURL = 'https://api.telegram.org/bot{0}/setWebhook'.format(bottoken)
-    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     api_url = 'https://api.telegram.org/bot{0}/sendMessage'.format(bottoken)
 
 
     try:
 
         data = request.json()
-        headers = {
-            'Content-Type': 'application/json'
-        }
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
         message = {
             'chat_id': data['message']['chat']['id'],
             'text': data['message']['text']
