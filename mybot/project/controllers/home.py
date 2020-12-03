9c01956243c5a59caac2360ac4ce2a44621f026e
Nikolay Sitala <nsitala@ukr.net>
2020-12-03 18:20:06 +0200

    roz5

--- mybot/project/controllers/home.py
+++ mybot/project/controllers/home.py
@@ -8,6 +8,23 @@ import requests
 import logging
 
 
+def benchmark(mt):
+    def actual_decorator(func):
+        import time
+
+        def wrapper(*args, **kwargs):
+            total = 0
+            for i in range(mt):
+                start = time.time()
+                return_value = func(*args, **kwargs)
+                end = time.time()
+                total = total + (end - start)
+            logging.info('[*] Среднее время выполнения: {} секунд.'.format(total / mt))
+            return return_value
+        return wrapper
+    return actual_decorator
+
+
 @bottle.route('/')
 @view('index')
 def index():
@@ -25,6 +42,7 @@ def do_admin():
 
 
 @bottle.route('/api/v1/echo', method='POST')
+@benchmark(mt=1)
 def do_echo():
 
     bottoken = '528159377:AAEI3Y3zTYv18e2qBp_nXBBMxLZU1uUhPHg'
@@ -57,31 +75,3 @@ def do_echo():
     return '200'
 
 
-@bottle.route('/api/v2/echo', method='POST')
-def do_echo():
-
-    bottoken = '528159377:AAEI3Y3zTYv18e2qBp_nXBBMxLZU1uUhPHg'
-    api_url = 'https://api.telegram.org/bot{0}/sendMessage'.format(bottoken)
-
-    # Добавляем клавиатуру
-    reply_markup = {"keyboard": [[{"text": "1"}], [{"text": "2"}], [{"text": "3"}]],
-                    "resize_keyboard": True,
-                    "one_time_keyboard": False}
-
-    try:
-        data = request.json
-        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        message = {
-            'chat_id': data['message']['chat']['id'],
-            'text': "".join(['эхо', "_", str(data['message']['text'])]),
-            'reply_markup': reply_markup
-        }
-
-        r = requests.post(api_url, data=json.dumps(message), headers=headers)
-
-        assert r.status_code == 200
-
-    except Exception as ex:
-        logging.info(str(ex))
-        return '500'
-    return '200'
