f05f79748a4edcfa0d1bcf431d5f7cf9822a820b
Nikolay Sitala <nsitala@ukr.net>
2020-12-04 15:12:59 +0200

    redis

--- mybot/project/controllers/home.py
+++ mybot/project/controllers/home.py
@@ -3,9 +3,12 @@ import bottle
 from bottle import view, request, redirect
 from mybot.project.controllers import mail
 
+import os
 import json
 import requests
 import logging
+
+import redis
 
 # @bottle.route('/api/v1/echo', method='POST')
 # @benchmark(mt=1)
@@ -57,11 +60,14 @@ def do_admin():
     redirect('/')
 
 
+# InlineKeyboardButton
+
 def dispatch():
     def actual_decorator(func):
         def wrapper(*args, **kwargs):
+
             return_value = func(*args, **kwargs)
-            logging.info('ok'*40)
+            logging.info('ok'*10)
             return return_value
         return wrapper
     return actual_decorator
@@ -69,13 +75,16 @@ def dispatch():
 
 @bottle.route('/api/v1/echo', method='POST')
 @dispatch()
-def do_echo():
+def do_echo(data):
+
+    r = redis.from_url(os.environ.get("REDIS_URL"))
+    logging.info(f'Redis {r}')
 
     bottoken = '528159377:AAEI3Y3zTYv18e2qBp_nXBBMxLZU1uUhPHg'
     api_url = 'https://api.telegram.org/bot{0}/sendMessage'.format(bottoken)
 
     # Добавляем клавиатуру
-    reply_markup = {"keyboard": [[{"text": "1"}], [{"text": "2"}], [{"text": "3"}]],
+    reply_markup = {"keyboard": [[{"text": "Регион"}], [{"text": "2"}], [{"text": "3"}]],
                     "resize_keyboard": True,
                     "one_time_keyboard": False}
 
@@ -99,5 +108,14 @@ def do_echo():
         logging.info(str(ex))
         return '500'
     return '200'
+
+
+
+# 'message': {'message_id': 425,
+#             'from': {'id': 471125560, 'is_bot': False, 'first_name': 'Kolya', 'last_name': 'Sitala', 'language_code': 'ru'},
+#             'chat': {'id': 471125560, 'first_name': 'Kolya', 'last_name': 'Sitala', 'type': 'private'},
+#             'date': 1607086983,
+#             'text': '1'}
+
 
 
