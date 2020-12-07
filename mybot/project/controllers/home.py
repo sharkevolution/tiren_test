26d9b56fc5c53dd02612e6666b3cabb22bf9b0e4
Nikolay Sitala <nsitala@ukr.net>
2020-12-07 18:05:05 +0200

    first

--- mybot/project/controllers/home.py
+++ mybot/project/controllers/home.py
@@ -208,7 +208,7 @@ def test2(*args, **kwargs):
 
 @dp.message_handler(commands=['Регион', ])
 def test3(*args, **kwargs):
-    ej_ukraine = emoji.emojize(':flag_for_Ukraine:')
+    ej_ukraine = emoji.emojize(':Ukraine:')
     ej_city = emoji.emojize(':cityscape:')
     reply_markup = {"inline_keyboard": [[
         {"text": f"Регион {ej_ukraine}", "callback_data": "region"},
