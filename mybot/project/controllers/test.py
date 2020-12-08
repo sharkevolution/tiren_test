578feb687cb283405a5ab951f509051143103be6
Nikolay Sitala <nsitala@ukr.net>
2020-12-08 13:18:42 +0200

    first

--- mybot/project/controllers/test.py
+++ mybot/project/controllers/test.py
@@ -1,24 +1,24 @@
 
-def callback_handler(commands):
-    def decorator(fn):
-        for b in commands:
-            pull_callback_commands = fn
-
-        def decorated2(*args, **kwargs):
-            return fn(*args, **kwargs)
-
-        decorated2.__name__ = fn.__name__
-        return decorated2
-
-    return decorator
-
-
-@callback_handler("new")
-def f(data):
-    print(data)
-
-data = {'key': 0}
-
-
-f(data)
+# def callback_handler(commands):
+#     def decorator(fn):
+#         for b in commands:
+#             pull_callback_commands = fn
+#
+#         def decorated2(*args, **kwargs):
+#             return fn(*args, **kwargs)
+#
+#         decorated2.__name__ = fn.__name__
+#         return decorated2
+#
+#     return decorator
+#
+#
+# @callback_handler("new")
+# def f(data):
+#     print(data)
+#
+# data = {'key': 0}
+#
+#
+# f(data)
 
