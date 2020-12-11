7fc65a10c4200df718532a3684f7164f6b364972
Nikolay Sitala <nsitala@ukr.net>
2020-12-11 21:22:29 +0200

    first

--- mybot/project/controllers/tebot1.py
+++ mybot/project/controllers/tebot1.py
@@ -130,7 +130,7 @@ def region_arrived(data):
     assert r.status_code == 200
 
     message = {
-        'chat_id': data['callback_query']['chat']['id'],
+        'chat_id': data['callback_query']['message']['chat']['id'],
         'text': '/bc',
     }
     return message, bot.api_url
