7ca51039734d839b21a46409e6d76c5f30b879fd
Nikolay Sitala <nsitala@ukr.net>
2020-12-14 19:13:00 +0200

    first

--- mybot/project/controllers/tebot1.py
+++ mybot/project/controllers/tebot1.py
@@ -113,19 +113,6 @@ bot = Bot(API_TOKEN)
 dp = Dispatcher(bot)
 
 
-# @dp.callback_handler(commands=['region_arrived', ])
-# def region_arrived(data):
-#
-#     callback_hello_ok(data, 'Выберите адрес')
-#
-#     tunel = data['callback_query']['message']['chat']['id']
-#     result_text = 'Введите время прибытия и выберите перевозчика из списка'
-#     reply_markup = settings_user.template_engineer_mode()
-#     message = {'chat_id': tunel, 'text': result_text, 'reply_markup': reply_markup}
-#
-#     return message, bot.api_url
-
-
 @dp.message_handler(commands=HANDLER_USER_ADR)
 def bind_bot(data):
     tunel = data['message']['chat']['id']
