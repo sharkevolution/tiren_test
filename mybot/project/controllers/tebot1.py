39979ade57e7650347118a5344b2af9bf4e7e1da
Nikolay Sitala <nsitala@ukr.net>
2020-12-15 22:58:26 +0200

    first

--- mybot/project/controllers/tebot1.py
+++ mybot/project/controllers/tebot1.py
@@ -280,7 +280,9 @@ def keboard_bot(data):
     # Input pass
     chat_user = bot.users[tunnel]
     chat_user.combination = []
-    bot.users.users[tunnel] = chat_user
+
+    bot.users[tunnel] = chat_user
+
     result_text = "Input key: "
     message = {'chat_id': data['message']['chat']['id'], 'text': result_text}
 
