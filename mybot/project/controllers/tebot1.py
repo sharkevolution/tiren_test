3951eceaa531c0ad63258cc6120c600211f871ee
Nikolay Sitala <nsitala@ukr.net>
2020-12-16 13:21:34 +0200

    first

--- mybot/project/controllers/tebot1.py
+++ mybot/project/controllers/tebot1.py
@@ -234,13 +234,15 @@ def enter(data, ord=None):
                   'ent_nine': 9, 'ent_zero': 0, 'ent_colon': ':'}
 
     valid_range = {'01': 0, '02': 0, '03': 0, '04': 0, '05': 0, '06': 0, '07': 0,
-                   '08': 0, '09': 0, '1': 0, '2': 0, '10': 0, '11': 0, '12': 0, '13': 0,
-                   '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0}
+                   '08': 0, '09': 0, '1': 0, '2': 0, '10': 0, '11': 0, '12': 0,
+                   '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0,
+                   '20': 0, '21': 0, '22': 0, '23': 0}
 
     # Edit Message
     chat_user = bot.users[chat_id]
 
     tmp_list = chat_user.combination
+    tmp_list.append(str(number_key[ord]))
     raw_text = ''.join(tmp_list)
 
     logging.info(raw_text)
