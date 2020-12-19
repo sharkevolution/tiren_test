a94462d7a1ef3ee8866caa5762f4951ea91d4d90
Nikolay Sitala <nsitala@ukr.net>
2020-12-19 12:13:37 +0200

    first

--- mybot/project/controllers/test.py
+++ mybot/project/controllers/test.py
@@ -115,74 +115,74 @@
 # f = open()
 # hhhh(f.g)
 
-import copy
-
-number_key = {'ent_one': 1, 'ent_two': 2, 'ent_three': 3, 'ent_four': 4,
-              'ent_five': 5, 'ent_six': 6, 'ent_seven': 7, 'ent_eight': 8,
-              'ent_nine': 9, 'ent_zero': 0, 'ent_colon': ':'}
-
-valid_range = {'01': 1, '02': 1, '03': 1, '04': 1, '05': 1, '06': 1, '07': 1,
-               '08': 1, '09': 1, '10': 1, '11': 1, '12': 1, '13': 1, '14': 1,
-               '15': 1, '16': 1, '17': 1, '18': 1, '19': 1, '20': 1, '21': 1,
-               '22': 1, '23': 1, '1': 1, '2': 1, '3': 1, '4': 1, '5': 1, '6': 1,
-               '7': 1, '8': 1, '9': 9, '0': 0}
+# import copy
 #
+# number_key = {'ent_one': 1, 'ent_two': 2, 'ent_three': 3, 'ent_four': 4,
+#               'ent_five': 5, 'ent_six': 6, 'ent_seven': 7, 'ent_eight': 8,
+#               'ent_nine': 9, 'ent_zero': 0, 'ent_colon': ':'}
 #
-def my(ord, comb):
-
-    tmp_list = copy.deepcopy(comb)
-    # if str(number_key[ord]) in valid_range:
-
-    if ':' in tmp_list:
-        ind = tmp_list.index(':')
-        left_side = tmp_list[:ind]
-        right_side = tmp_list[ind + 1:]
-
-        if right_side:
-            if len(right_side) == 1:
-                if str(number_key[ord]) in '0123456789':
-                    right_side.append(str(number_key[ord]))
-        else:
-            if str(number_key[ord]) in '012345':
-                right_side.append(str(number_key[ord]))
-
-        tmp_list = left_side + [':'] + right_side
-        #print(tmp_list)
-
-    else:
-        left_side = []
-        if len(tmp_list) == 1:
-            if int(tmp_list[0]) == 2:
-                if str(number_key[ord]) in '0123:':
-                    left_side.append(str(number_key[ord]))
-            else:
-                if tmp_list[0] in '3456789':
-                    pass
-                else:
-                    left_side.append(str(number_key[ord]))
-            if not ':' == str(number_key[ord]):
-                left_side.append(':')
-            tmp_list += left_side
-            if len(tmp_list) == 2:
-                tmp_list.insert(0, '0')
-        elif len(tmp_list) == 2:
-            left_side.append(':')
-            tmp_list += left_side
-        else:
-            if str(number_key[ord]) in '0123456789':
-                left_side.append(str(number_key[ord]))
-            tmp_list += left_side
-        #print(tmp_list)
-
-    comb = copy.deepcopy(tmp_list)
-
-    return comb
-
-#y = my('ent_three', [])
-y1 = my('ent_four', ['2',])
-
-#print(''.join(y))
-print(''.join(y1))
+# valid_range = {'01': 1, '02': 1, '03': 1, '04': 1, '05': 1, '06': 1, '07': 1,
+#                '08': 1, '09': 1, '10': 1, '11': 1, '12': 1, '13': 1, '14': 1,
+#                '15': 1, '16': 1, '17': 1, '18': 1, '19': 1, '20': 1, '21': 1,
+#                '22': 1, '23': 1, '1': 1, '2': 1, '3': 1, '4': 1, '5': 1, '6': 1,
+#                '7': 1, '8': 1, '9': 9, '0': 0}
+# #
+# #
+# def my(ord, comb):
+#
+#     tmp_list = copy.deepcopy(comb)
+#     # if str(number_key[ord]) in valid_range:
+#
+#     if ':' in tmp_list:
+#         ind = tmp_list.index(':')
+#         left_side = tmp_list[:ind]
+#         right_side = tmp_list[ind + 1:]
+#
+#         if right_side:
+#             if len(right_side) == 1:
+#                 if str(number_key[ord]) in '0123456789':
+#                     right_side.append(str(number_key[ord]))
+#         else:
+#             if str(number_key[ord]) in '012345':
+#                 right_side.append(str(number_key[ord]))
+#
+#         tmp_list = left_side + [':'] + right_side
+#         #print(tmp_list)
+#
+#     else:
+#         left_side = []
+#         if len(tmp_list) == 1:
+#             if int(tmp_list[0]) == 2:
+#                 if str(number_key[ord]) in '0123:':
+#                     left_side.append(str(number_key[ord]))
+#             else:
+#                 if tmp_list[0] in '3456789':
+#                     pass
+#                 else:
+#                     left_side.append(str(number_key[ord]))
+#             if not ':' == str(number_key[ord]):
+#                 left_side.append(':')
+#             tmp_list += left_side
+#             if len(tmp_list) == 2:
+#                 tmp_list.insert(0, '0')
+#         elif len(tmp_list) == 2:
+#             left_side.append(':')
+#             tmp_list += left_side
+#         else:
+#             if str(number_key[ord]) in '0123456789':
+#                 left_side.append(str(number_key[ord]))
+#             tmp_list += left_side
+#         #print(tmp_list)
+#
+#     comb = copy.deepcopy(tmp_list)
+#
+#     return comb
+#
+# #y = my('ent_three', [])
+# y1 = my('ent_nine', ['7',])
+#
+# #print(''.join(y))
+# print(''.join(y1))
 
 
 
