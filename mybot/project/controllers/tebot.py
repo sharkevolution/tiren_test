d47894795a31a378a3d742d32fc91838c85cafd7
Nikolay Sitala <nsitala@ukr.net>
2020-12-23 09:39:31 +0200

    first

--- mybot/project/controllers/tebot.py
+++ mybot/project/controllers/tebot.py
@@ -710,22 +710,21 @@ def do_echo():
                 logging.info(ord)
                 logging.info(chat_user.call_fsm)
 
+                if exec_func := chat_user.pull_user_commands.get(ord):
+                    message, curl = exec_func(data, ord)
+                elif exec_func := dp.pull_message_commands.get(ord):
+                    # logging.info(ord)
+                    message, curl = exec_func(data, ord)
+                else:
+                    message, curl = dummy_message(data)
+
                 if chat_user.FSM:
-                    if exec_func := chat_user.pull_user_commands.get(ord):
+                    if exec_func := dp.pull_message_commands.get(ord):
                         chat_user.FSM = False
                         chat_user.previous_ord = None
                         chat_user.call_fsm = None
                         logging.info('Bad FSM')
                         # Сообщение что ожидался ввод строки
-
-                        if exec_func := chat_user.pull_user_commands.get(ord):
-                            message, curl = exec_func(data, ord)
-                        elif exec_func := dp.pull_message_commands.get(ord):
-                            # logging.info(ord)
-                            message, curl = exec_func(data, ord)
-                        else:
-                            message, curl = dummy_message(data)
-
                     else:
                         # Start FSM
                         chat_user.call_fsm(data, ord)
