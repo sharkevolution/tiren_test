29977e818f48c488e77197699aaa789d5d2e33ed
Nikolay Sitala <nsitala@ukr.net>
2020-12-23 21:24:32 +0200

    first

--- mybot/project/controllers/treeadr.py
+++ mybot/project/controllers/treeadr.py
@@ -4,6 +4,25 @@ import logging
 
 def show_city(tree_city, chat_id, name_ord):
 
+    single_quote = '\''
+    city_name = name_ord.split(f"{single_quote}")
+
+    logging.info(city_name)
+    
+    for b in tree_city:
+        if city_name[1] in b[1]:
+            access = b[2]
+            if chat_id in access:
+                pass
+            else:
+                access.append(chat_id)
+                b[2] = access
+
+    return tree_city
+
+
+def hide_city(tree_city, chat_id, name_ord):
+
     for b in tree_city:
         if name_ord in b[1]:
             access = b[2]
