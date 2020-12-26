91d6f2f9cee6098355a8acb61ef4c6b2a6720a5f
Nikolay Sitala <nsitala@ukr.net>
2020-12-26 12:17:05 +0200

    first

--- mybot/project/controllers/tebot.py
+++ mybot/project/controllers/tebot.py
@@ -261,7 +261,6 @@ def fsm_city(data, ord=None):
             if new_city_split[idx].lower() == b[1].lower():
                 dup_city = False
                 logging.info("dup_city = false")
-                logging.info(b)
                 max_key_city = b[0]
                 break
 
