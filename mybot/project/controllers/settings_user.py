184fcb539b12506a1db4f0eff4e1895540775a06
sharkevolution <nsitala@gmail.com>
2021-01-29 13:37:04 +0200

    first

--- mybot/project/controllers/settings_user.py
+++ mybot/project/controllers/settings_user.py
@@ -243,6 +243,7 @@ def template_sub_datetime(bot, ord):
 
             if username == ord:
                 for userdata in bot.subscription[b]:
+                    logging.info(userdata)
                     users_messages.append([{"text": userdata[0]}])
                     commands_.append([userdata[0]])
         else:
