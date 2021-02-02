5f2332ef6bd9777ee9e338ece7df1d00a4bcb95b
sharkevolution <nsitala@gmail.com>
2021-02-02 15:57:56 +0200

    first

--- mybot/project/controllers/tebot.py
+++ mybot/project/controllers/tebot.py
@@ -753,7 +753,6 @@ def dynamic_sub_data(data, ord=None):
     result_text = f'{ord}'
 
     reply_markup, result_text = settings_user.template_sub_print(bot, ord)
-    result_text = reply_markup
 
     message = {'chat_id': tunnel, 'text': result_text}
     return message, bot.api_url
