dc281368d1cc4004e5679588461c627d08a47478
sharkevolution <nsitala@gmail.com>
2021-02-02 11:43:18 +0200

    first

--- mybot/project/controllers/settings_user.py
+++ mybot/project/controllers/settings_user.py
@@ -241,7 +241,7 @@ def template_sub_print(bot, ord):
             if username == ord:
                 for userdata in bot.subscription[b]:
 
-                    logging.info(userdata[1])
+                    logging.info(bot.subscription[userdata])
                     users_text.append([{"text": userdata[0]}])
                     commands_.append(userdata[0])
         else:
