6e6f30e8d9777f0413753e4e8ec2ac49e559ff58
sharkevolution <nsitala@gmail.com>
2021-02-02 15:36:04 +0200

    first

--- mybot/project/controllers/settings_user.py
+++ mybot/project/controllers/settings_user.py
@@ -237,11 +237,12 @@ def template_sub_print(bot, ord):
 
     if uid := bot.subscription.get(bot.selected_subscriber):
         for chunk in uid:
+            logging.info('CHUNK')
             logging.info(chunk)
             logging.info(ord)
+            logging.info('CHUNK')
 
             if chunk[0] == ord:
-                logging.info(chunk)
                 for k in chunk:
                     users_text.append([{"text": k}])
                     commands_.append(k)
