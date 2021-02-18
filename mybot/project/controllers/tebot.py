131e93e224f1a464023dc242560ff9fb8219c730
sharkevolution <nsitala@gmail.com>
2021-02-18 18:00:19 +0200

    first

--- mybot/project/controllers/tebot.py
+++ mybot/project/controllers/tebot.py
@@ -351,7 +351,7 @@ def fsm_city(data, ord=None):
             logging.info(new_adr)
             logging.info(adr_)
             max_key_address = int(max_key_address) + 1
-            adr_.append([str(max_key_city), str(max_key_address + 1), new_adr, []])
+            adr_.append([str(max_key_city), str(max_key_address), new_adr, []])
             rev_adr = sorted(adr_, key=lambda num: num[1], reverse=True)
             bot.dict_init['adr'] = rev_adr
 
