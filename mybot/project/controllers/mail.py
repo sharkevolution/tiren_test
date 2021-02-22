a4f776adcae5ac74e7feb4a45bef39bde9d6908a
sharkevolution <nsitala@gmail.com>
2021-02-22 17:37:05 +0200

    first

--- mybot/project/controllers/mail.py
+++ mybot/project/controllers/mail.py
@@ -1,43 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+import os
+import logging
+
 import requests
 import gmail
 from gmail import Message
 
 
 def send_mail(base_mail, to_mail, user_name, user_text):
-    # try:
     b = "".join(["Thanks 2018! <", base_mail, ">"])
 
-    gm = gmail.GMail(b, 'fortuna-3#')
-    msg = Message('Спасибо от', to="me <{0}>".format(to_mail),
-                  text="Спасибо от {0}, сообщение: {1}".format(user_name, user_text))
-    gm.send(msg)
-    # except Exception as ex:
-    #     pass
-
-# def send_mail_key(base_mail, base_mailpass, to_mail, akey):
-#
-#     b = "".join(["sharkevo <", base_mail, ">"])
-#     txt = "".join(["Для завершения регистрации и начала работы ",
-#                    "введите указанный код активации на сайте: ",
-#                    akey])
-#
-#     gm = gmail.GMail(b, base_mailpass)
-#     msg = Message('Завершение регистрации', to="me <{0}>".format(to_mail), text=txt)
-#     gm.send(msg)
-
-    "tgxm8ou"
+    if GMAIL_PASS := os.environ.get("GMAIL_PASS"):
+        gm = gmail.GMail(b, GMAIL_PASS)
+        msg = Message('Спасибо от', to="me <{0}>".format(to_mail),
+                      text="Спасибо от {0}, сообщение: {1}".format(user_name, user_text))
+        gm.send(msg)
 
 
 def send_sms(user_name, user_text):
 
-    try:
-
-        # Отправка сообщения пользователю
+    if password := os.environ.get("SMS_PASS"):
         login = '380732218247'  # phone number
-        password = 'tgxm8ou'  # Password
         alphaName = 'gsm1'  # string, sender id (alpha-name)
 
         abonent = '380732218247'
@@ -53,10 +38,5 @@ def send_sms(user_name, user_text):
 
         try:
             res = requests.post(url, data=xml, headers=headers)
-        except:
-            pass
-            # print(res.status_code)
-            # print(res.raise_for_status())
-
-    except Exception as ex:
-        pass
+        except Exception as ex:
+            logging.info(ex)
