ba6fdf98ff1a127144830389aa4ff7384ab8ae50
sharkevolution <nsitala@gmail.com>
2021-03-01 17:15:44 +0200

    first

--- mybot/project/controllers/dredis.py
+++ mybot/project/controllers/dredis.py
@@ -58,6 +58,7 @@ def clear_base_redis():
     # Clear base Redis
     redisClient = redis.from_url(os.environ.get("REDIS_URL"))
     for key in redisClient.keys('*'):
+        logging.info(key)
         redisClient.delete(key)
 
 
