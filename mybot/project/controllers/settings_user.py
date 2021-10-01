c53b73a12974e22e4b1a60ae33bdc7c92548ba68
sharkevolution <nsitala@gmail.com>
2021-10-01 16:51:09 +0300

    base

--- mybot/project/controllers/settings_user.py
+++ mybot/project/controllers/settings_user.py
@@ -579,16 +579,16 @@ def template_user_delivery(dict_init, chat_user):
     dlv = []
 
     for b in dict_init['user_delivery']:
+        if chat_user.__name__ == b[0]:
+            for h in dict_init['delivery']:
+                if b[1] == h[0]:
+                    name_delivery = h[1]
+                    logging.info(name_delivery)
 
-        for h in dict_init['delivery']:
-            if b[1] == h[0]:
-                name_delivery = h[1]
-                logging.info(name_delivery)
-    
-                dlv.append(
-                    {"text": name_delivery}
-                )
-                chat_user.delivery.append(name_delivery)
+                    dlv.append(
+                        {"text": name_delivery}
+                    )
+                    chat_user.delivery.append(name_delivery)
 
     n = 2
     resize_dlv = [dlv[i:i + n] for i in range(0, len(dlv), n)]
