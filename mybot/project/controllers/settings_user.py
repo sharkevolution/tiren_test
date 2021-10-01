130f5db30843a89a8a8d1a6c6e6ed72c6aeb600b
sharkevolution <nsitala@gmail.com>
2021-10-01 16:57:26 +0300

    base

--- mybot/project/controllers/settings_user.py
+++ mybot/project/controllers/settings_user.py
@@ -580,7 +580,7 @@ def template_user_delivery(dict_init, chat_user):
 
     for b in dict_init['user_delivery']:
         logging.info(f'{chat_user.__name__} == {b[0]}')
-        if chat_user.__name__ == b[0]:
+        if chat_user.__name__ == int(b[0]):
             for h in dict_init['delivery']:
                 if b[1] == h[0]:
                     name_delivery = h[1]
